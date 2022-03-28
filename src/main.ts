a170f86c1d75df71dda6533198921685df79f27d
Jayanthbharadwaj <jayanthbharadwajm@gmail.com>
2022-03-28 18:36:35 +0530

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { createConnections } from 'typeorm';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  try {
+    await createConnections();
+  } catch (error) {
+    console.log('Error in DB connection', error);
+  }
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setGlobalPrefix('/api');
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
