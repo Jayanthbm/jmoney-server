f2c44a8e1f72d55d7ea3ddff925674151d17541c
Jayanthbharadwaj <jayanthbharadwajm@gmail.com>
2022-03-28 18:39:05 +0530

    Default constants Added

---
+++ src/shared/helpers/constants.ts
@@ -0,0 +1,82 @@
+export const jwtConstants = {
+  secret: 'jayanth@123',
+};
+
+export const defaultIncomeCategories = [
+  {
+    name: 'Salary',
+    icon: 'fas fa-dollar-sign',
+  },
+  {
+    name: 'Freelancing',
+    icon: 'fas fa-hand-holding-usd',
+  },
+  {
+    name: 'Other',
+    icon: 'fas fa-money-bill-wave',
+  },
+];
+
+export const defaultExpenseCategories = [
+  {
+    name: 'Home',
+    icon: 'fas fa-home',
+  },
+  {
+    name: 'Loans',
+    icon: 'fas fa-hand-holding-usd',
+  },
+
+  {
+    name: 'Food',
+    icon: 'fas fa-utensils',
+  },
+  {
+    name: 'Drinks',
+    icon: 'fas fa-wine-glass-alt',
+  },
+  {
+    name: 'Bills',
+    icon: 'fas fa-file-invoice-dollar',
+  },
+  {
+    name: 'Fuel',
+    icon: 'fas fa-gas-pump',
+  },
+  {
+    name: 'Transportation',
+    icon: 'fas fa-bus-alt',
+  },
+  {
+    name: 'Entertainment',
+    icon: 'fas fa-grin-beam',
+  },
+  {
+    name: 'Shopping',
+    icon: 'fas fa-shopping-cart',
+  },
+  {
+    name: 'Personal',
+    icon: 'fas fa-user-tie',
+  },
+  {
+    name: 'Repairs',
+    icon: 'fas fa-tools',
+  },
+  {
+    name: 'Sports',
+    icon: 'fas fa-futbol',
+  },
+  {
+    name: 'Health',
+    icon: 'fas fa-heartbeat',
+  },
+  {
+    name: 'Gifts',
+    icon: 'fas fa-gift',
+  },
+  {
+    name: 'Other',
+    icon: 'fas fa-money-bill-wave',
+  },
+];
